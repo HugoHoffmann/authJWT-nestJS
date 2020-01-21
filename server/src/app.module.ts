91a3af0a543e01ced141d516680cf05df49dc96e
hugohoffmann <hugohoffmann04@gmail.com>
2020-01-21 19:36:35 -0300

    new: structure users

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
