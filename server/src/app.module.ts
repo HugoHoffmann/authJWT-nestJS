b2478117a42c203c91cd82d48ab375cbf6d9b897
hugohoffmann <hugohoffmann04@gmail.com>
2020-01-21 23:01:20 -0300

    chore: rename user.controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './UserModule/users.module';
-import { UsersController } from './UserModule/controllers/users.controller';
+import { UsersController } from './UserModule/controllers/user.controller';
 import { UsersService } from './UserModule/service/user.service';
 
 @Module({
