f3587c31d4d8e793ac7bc3fcaab589d5055d3422
hugohoffmann <hugohoffmann04@gmail.com>
2020-01-22 23:46:26 -0300

    new: userEntity

---
+++ server/src/UserModule/entity/user.entity.ts
@@ -0,0 +1,33 @@
+import * as crypto from 'crypto'
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Audit } from '../../CommonsModule/entity'
+
+Entity()
+export class User extends Audit{
+
+    @PrimaryGeneratedColumn('increment')
+    id: number
+
+    @Column({
+        nullable: false
+    })
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column({
+        default: '',
+      })
+    salt: string
+
+    validPassword(password: string){
+        const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex')
+        /** case password is valid */
+        return this.password === hash
+    }
+
+}
