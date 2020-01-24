af87068e6990b3f6d60ef140936dc830bfc07278
hugohoffmann <hugohoffmann04@gmail.com>
2020-01-23 23:18:39 -0300

    new: user repository

---
+++ server/src/UserModule/repository/user.repository.ts
@@ -0,0 +1,11 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { User } from '../entity'
+
+@EntityRepository()
+export class UserRepository extends Repository<User> {
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        return this.findOne({ email })
+    }
+    
+}
